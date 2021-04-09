1ff311cd0d0dda862037233b1ae4c9d1af7afdf2
PatrickDD <1229579784@qq.com>
2021-04-09 22:06:13 +0800

    Version 0.1

--- CompilerProject1/main.cpp
+++ CompilerProject1/main.cpp
@@ -13,6 +13,13 @@ map<string, int> KEY_WORDS_MAP;
 string code;
 int cur = 0;
 
+int row_count = 1;
+// int word_count = 0;
+
+vector<pair<string, int> > lex_result;
+
+map<string, int> word_type_count;
+
 
 void init_key_words_map(){
     for(int i=0; i<KEY_WORDS.size(); i++){
@@ -20,9 +27,6 @@ void init_key_words_map(){
     }
 }
 
-//int check(string key){
-//    return KEY_WORDS_MAP[key];
-//}
 
 void init_code(string code_path){
     ifstream infile;
@@ -44,7 +48,7 @@ bool is_digit(char c){
 }
 
 bool is_hexademical_digit(char c){
-    return (c >= '0' && c <='9') || (c>='A' && c <='F');
+    return (c >= '0' && c <='9') || (c>='A' && c <='F') || (c>='a' && c <='f');
 }
 
 bool is_octal_digit(char c){
@@ -66,15 +70,23 @@ bool is_operator(char c){
 }
 
 string check_identifier(int s){
-//    int s = cur_r;
     int n = 1;
-    while(is_letter(code[s+n]) || is_digit(code[s+n])){
-        n++;
+    
+    //for checking "else if"
+    if(code.substr(s, 7) == "else if"){
+        n = 7;
     }
     
+    else{
+        while(is_letter(code[s+n]) || is_digit(code[s+n])){
+            n++;
+        }
+    }
     cur = s + n;
+    
     return code.substr(s, n);
 }
+
 
 string check_decimal_number(int s){
     int n = 1;
@@ -82,10 +94,17 @@ string check_decimal_number(int s){
     while(is_digit(code[s+n])){
         n++;
     }
+    
+    //unqualified identifier like '3aa'
+    if(is_letter(code[s+n])){
+        printf("[ERROR] unqualified identifier at row [%d]\n", row_count);
+        exit(0);
+    }
 
     cur = s + n;
     return code.substr(s, n);
 }
+
 
 string check_hexademical_number(int s){
     int n=1;
@@ -102,6 +121,7 @@ string check_hexademical_number(int s){
     return code.substr(s, n);
 }
 
+
 string check_octal_number(int s){
     int n=1;
     while(is_octal_digit(code[s+n])){
@@ -110,6 +130,7 @@ string check_octal_number(int s){
     cur = s + n;
     return code.substr(s, n);
 }
+
 
 string check_operator(int s){
     int n=1;
@@ -143,12 +164,8 @@ string check_operator(int s){
     return code.substr(s, n);
 }
 
-void cout_info(string word, int id){
-    cout << "< " << word<<"  "<<id <<" >" << endl;
-}
 
-
-void handle(){
+void lexical_analysis(){
     init_key_words_map();
     
     init_code("p1.txt");
@@ -158,8 +175,10 @@ void handle(){
         
         if(is_space(c)){
             cur++;
+            if(c=='\n'){
+                row_count++;
+            }
         }
-        
         
         //handle identifier or key word
         else if(is_letter(c)){
@@ -168,12 +187,14 @@ void handle(){
             
             //key_id is not 0, Key Word
             if(key_id!=0){
-                cout_info(id, key_id);
+                // cout_info(id, key_id);
+                lex_result.push_back(make_pair(id, key_id));
             }
             
             //key_id is 0, Identifier
             else{
-                cout_info(id, KEY_WORDS_MAP["Identifier"]);
+                // print_info(id, KEY_WORDS_MAP["Identifier"]);
+                lex_result.push_back(make_pair(id, KEY_WORDS_MAP["Identifier"]));
             }
         }
         
@@ -185,19 +206,23 @@ void handle(){
             //decimal number : 123, 0
             if(c!='0' || (c == '0' && !(next == 'x' || next == 'X' || is_octal_digit(next)))){
                 string decimal_num = check_decimal_number(cur);
-                cout_info(decimal_num, KEY_WORDS_MAP["Decimal_Number"]);
+                // print_info(decimal_num, KEY_WORDS_MAP["Decimal_Number"]);
+                lex_result.push_back(make_pair(decimal_num, KEY_WORDS_MAP["Decimal_Number"]));
             }
+            
             else{
                 //hexademical number : 0xAF, 0X111F
                 if(next == 'x' || next == 'X'){
                     string hexademical_num = check_hexademical_number(cur);
-                    cout_info(hexademical_num, KEY_WORDS_MAP["Hexademical_Number"]);
+                    // print_info(hexademical_num, KEY_WORDS_MAP["Hexademical_Number"]);
+                    lex_result.push_back(make_pair(hexademical_num, KEY_WORDS_MAP["Hexademical_Number"]));
                 }
                 
                 //octal number : 012, 077
                 else{
                     string octal_num = check_octal_number(cur);
-                    cout_info(octal_num, KEY_WORDS_MAP["Octal_Number"]);
+                    // print_info(octal_num, KEY_WORDS_MAP["Octal_Number"]);
+                    lex_result.push_back(make_pair(octal_num, KEY_WORDS_MAP["Octal_Number"]));
                 }
             }
         }
@@ -206,17 +231,48 @@ void handle(){
         //handle operator (beginning with <, =, ...)
         else if(is_operator(c)){
             string op = check_operator(cur);
-            cout_info(op, KEY_WORDS_MAP[op]);
+            // print_info(op, KEY_WORDS_MAP[op]);
+            lex_result.push_back(make_pair(op, KEY_WORDS_MAP[op]));
         }
         
-        else{
-            cur++;
-        }
+//        else{
+//            cur++;
+//        }
     }
+
+
+    printf("[INFO] results of the lexical analysis : \n");
+    for(auto i : lex_result){
+        string word = i.first;
+        int id = i.second;
+        // print_info(word, id);
+        printf("< %s  %d >\n", word.c_str(), id);
+        
+        //get the word_type("Identifier", ...) through KEY_WORDS[id-1]
+        word_type_count[KEY_WORDS[id-1]]++;
+    }
+    printf("-----------------------------------------\n\n");
+    
+
+    printf("[INFO] occurrences of different words :  \n");
+    for(auto i:word_type_count){
+        // cout << i.first << " : " << i.second << endl;
+        string word = i.first;
+        int count = i.second;
+        printf("%s : %d time(s)\n", word.c_str(), count);
+    }
+    printf("-----------------------------------------\n\n");
+    
+    
+
+    printf("[INFO] total : %lu word(s) , %d row(s)\n", lex_result.size(), row_count);
+    printf("-----------------------------------------\n\n");
+
+
 }
 
 int main(){
     
-    handle();
+    lexical_analysis();
 
 }
